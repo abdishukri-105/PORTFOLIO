ffee565208de455afaf5a70c3011ae6fb0a9ff0e
abdishukri <abdishukrimohamed105@gmail.com>
2023-02-02 12:41:54 +0300

    navbar responsive

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,10 +3,10 @@ import herImage2 from "../images/hero2.jpg"
 
 const Hero = () => {
     return (
-        <div className=" mt-12 grid place-items-center shadow-lg mr-4">
+        <div className=" mt-8 grid place-items-center mr-4">
           <img src={herImage2} alt=""  className=" border-slate-500 border-8 items-center h-40 w-40 rounded-full"/>
-          <p className=" leading-9  text-white text-xl p-5 text-center mt-2 mb-3">Hello, i'm <span class=" text-teal-500">Abdishukri</span> <br />
-            a fullStack software developer  from Nairobi, Kenya </p>
+          <article className=" text-white w-2/3 text-center p-5 mt-2 mb-3">Hello, i'm <span className=" text-teal-500 font-bold">Abdishukri Mohamed</span > , a aspiring full-stack software developer from Nairobi, Kenya <br />
+          I'm passionate about technology and constantly learning new skills to enhance my abilities.    I have a solid foundation in <span className=" font-bold"> HTML,CSS,JAVASCRIPT,REACT,BOOTSTRAP,TAILWIND currently learning Ruby</span> and am eager to apply my knowledge to real-world projects.Take a look at my portfolio and let's start a conversation about how I can contribute to your team.</article>
          
         </div>
       );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,56 @@
+import { useState } from "react";
+
 const Navbar = () => {
+    const [navbar, setNavbar] = useState(false);
+
     return ( 
      <header>
-      <nav className=" py-4  md:py-3 px-4   bg-gray-900 border border-gray-800 border-t-0 border-r-0 border-l-0 shadow-md text-white ">
-        <div className="container relative  flex flex-wrap items-center justify-between mt-1">
-          <a href="#hh"  className=" ml-3 text-2xl  font-bold  text-teal-500  sm:left-2 sm:text-2xl sm:uppercase">Abdishukri</a>
-          <ul className=" hidden text-base sm:flex sm:items-center">
-            <li>
-              <a href="#hh" className=" py-2 block md:p-2   hover:underline ">About</a>
-            </li>
-            <a href="#hh" className=" px-4 py-2  hover:underline ">Skills</a>
-            <a href="#hh" className=" px-4 py-2  hover:underline ">Project</a>
-            <a href="#hh" className=" px-4 py-2  hover:underline ">Contact</a>
-            <a href="#hh" className=" px-4 py-2  bg-teal-800  rounded-md hover:bg-teal-700 ">call me</a>
-            <a href="#hh" className=" px-4 py-2  hover:underline "></a>
-          </ul>
-          <h2 class="h-6 w-6 mr-8 cursor-pointer sm:hidden block">menu </h2>
-        </div>
-      </nav>
+        <nav className="w-full bg-gray-900 md:border-gray-800 md:border-b-2 md:border-t-0 md:border-r-0 md:border-l-0">
+            <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+                <div>
+                    <div className="flex items-center justify-between py-3 md:py-5 md:block">
+                        <a href="">
+                            <h2 className="text-2xl text-teal-500 italic font-bold">AM</h2>
+                        </a>
+                        <div className="md:hidden">
+                            <button className="p-2 text-teal-500 rounded-md outline-none focus:border-gray-400 focus:border" onClick={() => setNavbar(!navbar)}     >
+                                {navbar ? (
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6"  viewBox="0 0 20 20"  fill="currentColor" >
+                                        <path fillRule="evenodd"  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"  clipRule="evenodd"  />
+                                    </svg>
+                                  ) : (
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6"  fill="none"  viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}  >
+                                        <path   strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16"  />
+                                    </svg>
+                                  )}
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+                      navbar ? "block" : "hidden"}`} >
+                        <ul className="items-center justify-center text-white space-y-8 md:flex md:space-x-6 md:space-y-0 ">
+                            <li className="hover:text-teal-500  hover:underline">
+                                <a href="">Home</a>
+                            </li>
+                            <li className="hover:text-teal-500 hover:underline ">
+                                <a href="">About</a>
+                            </li>
+                            <li className="hover:text-teal-500 hover:underline">
+                                <a href="">skills</a>
+                            </li>
+                            <li className=" hover:text-teal-500 hover:underline ">
+                                <a href="">project</a>
+                            </li>
+                            <li className="hover:text-teal-500 hover:underline ">
+                                <a href="">contacts</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
     </header>   
      );
 }
