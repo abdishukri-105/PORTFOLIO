bbd5cb579761327078ee114a107fd8b0842809be
abdishukri <abdishukrimohamed105@gmail.com>
2023-02-03 12:08:56 +0300

    done with skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,38 +1,47 @@
 const Skills = () => {
     return ( 
-        <div className="h-screen">
+        <div className="">
             <h1 className="text-teal-500 text-3xl text-center underline"> Skills</h1>
-            <div className="md:flex md:justify-around  justify-center wrap mt-10 border border-gray-700 rounded-xl ml-2 mr-2 shadow-lg p-4">
-                <div className="text-white p-3 md:text-9xl shadow-xl  bg-gray-800  rounded-xl  ">
+              <h1 className="text-white text-3xl text-center mt-8">Technologies i have worked with</h1>
+            <div className="sm:grid  sm:grid-cols-4 grid gap-3 grid-cols-2  sm:gap-4   wrap mt-10 border border-gray-700 rounded-xl ml-6 mr-6 md:ml-40 md:mr-40 shadow-lg p-4">
+                <div className="text-white p-3 grid justify-center text-5xl shadow-xl  bg-gray-800  rounded-xl transition ease-in-out delay-600 duration-300 hover:-translate-y-1 hover:scale-110  ">
                    <ion-icon name="logo-html5" className=""></ion-icon>
                    <p className="text-xl text-center"> expert</p>
                 </div>
-                <div  className="text-white md:text-9xl p-3 shadow-xl  bg-gray-800  rounded-xl ">
-                  <ion-icon name="logo-css3"></ion-icon>
+                <div  className="text-white grid justify-center  text-5xl p-3 shadow-xl  bg-gray-800  rounded-xl transition ease-in-out delay-600 duration-300 hover:-translate-y-1 hover:scale-110">
+                  <ion-icon name="logo-css3" className="text-center"></ion-icon>
                   <p className="text-xl text-center"> expert</p>
                 </div>
-                <div  className="text-yellow-500 md:text-9xl p-3 shadow-xl  bg-gray-800  rounded-xl ">
+                <div  className="text-white grid justify-center text-5xl p-3 shadow-xl  bg-gray-800  rounded-xl transition ease-in-out delay-600 duration-300 hover:-translate-y-1 hover:scale-110">
+                    <ion-icon name="logo-github"></ion-icon> 
+                   <p className="text-xl text-center"> expert</p>
+                </div>
+                <div  className="text-yellow-500 grid justify-center text-5xl p-3 shadow-xl  bg-gray-800  rounded-xl transition ease-in-out delay-600 duration-300 hover:-translate-y-1 hover:scale-110">
                    <ion-icon name="logo-javascript" className="shadow-lg rounded-lg"></ion-icon>
                    <p className="text-xl text-center"> expert</p>
                 </div>
-                <div  className="text-yellow-500 md:text-9xl p-3 shadow-xl  bg-gray-800  rounded-xl ">
-                   <ion-icon name="logo-javascript" className="shadow-lg rounded-lg"></ion-icon>
-                   <p className="text-xl text-center"> expert</p>
-                </div>
-                <div  className="text-yellow-500 md:text-9xl   p-3 shadow-xl  bg-gray-800  rounded-xl ">
-                   <ion-icon name="logo-javascript" className="shadow-lg rounded-lg"></ion-icon>
-                   <p className="text-xl text-center"> intermediate</p>
-                </div>
-                <div  className="text-blue-400 md:text-9xl p-3 shadow-xl  bg-gray-800  rounded-xl">
+                <div  className="text-sky-500  justify-center text-5xl grid  p-3 shadow-xl  bg-gray-800  rounded-xl transition ease-in-out delay-600 duration-300 hover:-translate-y-1 hover:scale-110 ">
                    <ion-icon name="logo-react"></ion-icon>
-                   <p className="text-xl text-center hidden"> intermediate</p>
+                   <p className="text-xl "> intermediate</p>
                 </div>
-                <div  className="text-blue-400 text-4xl md:text-9xl w-1/3 flex justify-center place-items-center p-3 shadow-xl  bg-gray-800  rounded-xl">
+                <div  className="text-yellow-500 grid justify-center text-5xl p-3 shadow-xl  bg-gray-800  rounded-xl transition ease-in-out delay-600 duration-300 hover:-translate-y-1 hover:scale-110">
                    <ion-icon name="logo-react"></ion-icon>
-                   <p className="text-xl text-center hidden"> intermediate</p>
+                   <p className="text-xl "> intermediate</p>
                 </div>
+                <div  className="text-yellow-500 grid justify-center text-5xl p-3 shadow-xl  bg-gray-800  rounded-xl transition ease-in-out delay-600 duration-300 hover:-translate-y-1 hover:scale-110">
+                   <ion-icon name="logo-react"></ion-icon>
+                   <p className="text-xl "> intermediate</p>
+                </div>
+                <div  className="text-yellow-500 grid justify-center text-5xl p-3 shadow-xl  bg-gray-800  rounded-xl transition ease-in-out delay-600 duration-300 hover:-translate-y-1 hover:scale-110">
+                   <ion-icon name="logo-react"></ion-icon>
+                   <p className="text-xl "> intermediate</p>
+                </div>
+               
+               
             </div>
-        </div>
+          
+         </div> 
+        
      );
 }
  
