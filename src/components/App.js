63f62dbe62b1b7696da5c313ee4037e2b6679acb
abdishukri <abdishukrimohamed105@gmail.com>
2023-05-24 10:59:56 +0300

    footer done

--- src/components/App.js
+++ src/components/App.js
@@ -9,19 +9,11 @@ import Projects from "./Projects";
 import Footer from "./Footer";
 import Form from "./Form";
 import { Routes,Route , Router} from "react-router-dom";
+
 function App() {
   return (
 
-    // <>
-    // <Navbar />
-    //   <Routes>
-    //     <Route exact path="/" element={<Hero/>} />
-    //     <Route exact path="/skills" element={ <About />} />
-    //     <Route exact path="/projects" element={<Projects/>} />
-    //     <Route exact path="/contact" element={ <Form/>} />
-       
-    //   </Routes>
-    //   </>
+  
       <div className=" bg-gray-100 antialiased">
             
              <Navbar  />
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,44 @@
 const Footer = () => {
-    return (  
-        <footer class=" bg-black block sm:flex sm:justify-around text-white  p-4 sm:p-4 mt-20 border-gray-800 border-t-2 md:border-b-0 md:border-r-0 md:border-l-0">
-          <div className="text-white mb-2 ">
-           © 2023 : <span className="">Abdishukri mohamed</span>
-          </div>
-          <div >
-          <span> Tel: +2547 22 52 13 14</span> 
-          </div>
+  return (
+    <footer className="bg-black text-white p-4 mt-20 border-gray-800 border-t-2 md:border-b-0 md:border-r-0 md:border-l-0">
+      <div className="flex flex-col items-center justify-center md:flex-row md:justify-between">
+        <div className="text-lg mb-4">
+          &copy; 2023: <span className="font-bold">Abdishukri Mohamed</span>
+        </div>
 
-        </footer>
-    );
-}
- 
+        <div className="text-white font-bold mb-4">
+          <span>Tel: +2547 22 52 13 14</span>
+        </div>
+
+        <div className="flex justify-center md:justify-end space-x-4">
+          <a
+            href="https://github.com/abdishukri-105"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white p-2 rounded-full hover:cursor text-2xl"
+          >
+            <ion-icon name="logo-github"></ion-icon>
+          </a>
+          <a
+            href="https://twitter.com/AbdishukriMoh18"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white p-2 rounded-full hover:cursor text-2xl"
+          >
+            <ion-icon name="logo-twitter"></ion-icon>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/abdishukri-mohamed/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white p-2 rounded-full hover:cursor text-2xl"
+          >
+            <ion-icon name="logo-linkedin"></ion-icon>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
 export default Footer;
